71ea6dc237c46aa1c7194aca8d5eb558d3da78c2
makaimark <makaimark@gmail.com>
2016-10-09 20:20:32 +0200

    i created the button for residents, and basic modal function works

--- static/index.js
+++ static/index.js
@@ -8,27 +8,29 @@ function loadDoc() {
     if (this.readyState == 4 && this.status == 200) {
         var result = JSON.parse(this.responseText);
         var next = result["next"];
+        // $("#next").attr("href", next);
+        // var previous = result["previous"];
+        // $("#previous").attr("href", previous);
         var table = document.getElementById("table");
-
+        var data = ["name", "diameter", "climate", "gravity", "terrain", "surface_water", "population", "residents"];
         for ( var i = 0; i < result["results"].length; i++){
             var row = table.insertRow(i+1);
-            var cell0 = row.insertCell(0);
-            cell0.innerHTML = result["results"][i]["name"];
-            var cell1 = row.insertCell(1);
-            cell1.innerHTML = result["results"][i]["diameter"];
-            var cell2 = row.insertCell(2);
-            cell2.innerHTML = result["results"][i]["climate"];
-            var cell3 = row.insertCell(3);
-            cell3.innerHTML = result["results"][i]["gravity"];
-            var cell4 = row.insertCell(4);
-            cell4.innerHTML = result["results"][i]["terrain"];
-            var cell5 = row.insertCell(5);
-            cell5.innerHTML = result["results"][i]["surface_water"];
-            var cell6 = row.insertCell(6);
-            cell6.innerHTML = result["results"][i]["population"];
-            var cell7 = row.insertCell(7);
-            cell7.innerHTML = "Residents";
-                // result["results"][i]["residents"];
+            for ( var j = 0; j < 7 ; j++) {
+                var cell = row.insertCell(j);
+                cell.innerHTML = result["results"][i][data[j]];
+            }
+            var cell_residents = row.insertCell(7);
+            if (result["results"][i]["residents"].length == 0) {
+                cell_residents.innerHTML = "No unknown residents";
+            } else {
+                var bt = document.createElement('input');
+                bt.type = "button";
+                bt.value = "residents";
+                bt.setAttribute("class", "btn btn-primary");
+                bt.setAttribute("data-toggle", "modal");
+                bt.setAttribute("data-target", "#residentsModal");
+                cell_residents.appendChild(bt);
+            }
         }
     }
   };
@@ -42,8 +44,10 @@ $(document).ready(function () {
     type: "GET",
     url: "http://127.0.0.1:5000/",
     dataType: "jsonp",
-    success: loadDoc(),
-})
+    success: loadDoc()
+    });
+    // $("#next").click(loadDoc);
+    // $("#previous").click(loadDoc);
 });
 
 
